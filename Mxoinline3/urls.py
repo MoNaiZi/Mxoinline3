55939e4c5bf33d8f0c7407700234970db57e19fe
Your Name <you@example.com>
2018-05-18 08:46:10 +0800

    2018-5-18

--- Mxoinline3/urls.py
+++ Mxoinline3/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
 import xadmin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
+    #TemplateView.as_view会将template转换为view
+    url('^$',TemplateView.as_view(template_name='index.html'),name='index'),
+    url('^login/$',TemplateView.as_view(template_name='login.html'),name='login'),
 ]
+
